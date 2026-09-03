48c3c7fa2f19a79962488aa85d3cb283e64769d4
larissasantos-dev <larissa.santosdev7@gmail.com>
2026-09-02 22:26:04 -0300

    chore: inicializa projeto backend

---
+++ src/main/java/br/edu/ifsp/associacaogaia/AssociacaoGaiaApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.associacaogaia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssociacaoGaiaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssociacaoGaiaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifsp/associacaogaia/AssociacaoGaiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.associacaogaia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociacaoGaiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
